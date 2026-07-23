bf18c0823c717c1b48c353d5835145615b6ce9f5
Givikap <kaitosekiya@outlook.com>
2026-07-23 15:18:22 -0500

    Add second faster C++ solution for '122. Best Time to Buy and Sell Stock II' problem

--- medium/0122_best_time_to_buy_and_sell_stocks_ii/solution.cpp
+++ medium/0122_best_time_to_buy_and_sell_stocks_ii/solution.cpp
@@ -5,16 +5,9 @@ public:
   int maxProfit(std::vector<int> &prices) {
     int profit = 0;
 
-    for (size_t i{}; i < prices.size(); ++i) {
-      while (i < prices.size() - 1 && prices[i] > prices[i + 1])
-        ++i;
-
-      int buyPrice = prices[i];
-
-      while (i < prices.size() - 1 && prices[i] < prices[i + 1])
-        ++i;
-
-      profit += prices[i] - buyPrice;
+    for (size_t i = 1; i < prices.size(); ++i) {
+      if (prices[i] > prices[i - 1])
+        profit += prices[i] - prices[i - 1];
     }
 
     return profit;
