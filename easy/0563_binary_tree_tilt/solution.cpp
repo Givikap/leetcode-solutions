9c9896f3077f44e7e92d4093a14cc55ff8201d6e
Givikap <kaitosekiya@outlook.com>
2026-07-23 20:52:21 -0500

    Add third faster C++ solution for '563. Binary Tree Tilt' problem

--- easy/0563_binary_tree_tilt/solution.cpp
+++ easy/0563_binary_tree_tilt/solution.cpp
@@ -1,23 +1,25 @@
 #include "../../utils/cpp/nodes.hpp"
+#include <functional>
 
 class Solution {
 public:
   int findTilt(utils::TreeNode *root) {
     int tilt = 0;
-    subtreeSum(root, tilt);
+
+    std::function<int(utils::TreeNode *)> subtreeSum =
+        [&](utils::TreeNode *node) -> int {
+      if (!node)
+        return 0;
+
+      int leftSum = subtreeSum(node->left);
+      int rightSum = subtreeSum(node->right);
+
+      tilt += abs(leftSum - rightSum);
+
+      return node->val + leftSum + rightSum;
+    };
+
+    subtreeSum(root);
     return tilt;
-  }
-
-private:
-  int subtreeSum(utils::TreeNode *node, int &tilt) {
-    if (!node)
-      return 0;
-
-    int leftSum = subtreeSum(node->left, tilt);
-    int rightSum = subtreeSum(node->right, tilt);
-
-    tilt += abs(leftSum - rightSum);
-
-    return node->val + leftSum + rightSum;
   }
 };
