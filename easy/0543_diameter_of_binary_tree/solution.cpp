f871e2cdce29c238a73aaf6893a1ce34a59f1a85
Givikap <kaitosekiya@outlook.com>
2026-07-23 20:27:56 -0500

    Add second faster C++ solution for '543. Diameter of Binary Tree' problem

--- easy/0543_diameter_of_binary_tree/solution.cpp
+++ easy/0543_diameter_of_binary_tree/solution.cpp
@@ -1,23 +1,25 @@
 #include "../../utils/cpp/nodes.hpp"
+#include <functional>
 
 class Solution {
-private:
-  int dfs(utils::TreeNode *node, int &maxDiameter) {
-    if (!node)
-      return 0;
-
-    int leftHeight = dfs(node->left, maxDiameter);
-    int rightHeight = dfs(node->right, maxDiameter);
-
-    maxDiameter = std::max(maxDiameter, leftHeight + rightHeight);
-
-    return 1 + std::max(leftHeight, rightHeight);
-  }
-
 public:
   int diameterOfBinaryTree(utils::TreeNode *root) {
     int maxDiameter = 0;
-    dfs(root, maxDiameter);
+
+    std::function<int(utils::TreeNode *)> dfs =
+        [&](utils::TreeNode *node) -> int {
+      if (!node)
+        return 0;
+
+      int leftHeight = dfs(node->left);
+      int rightHeight = dfs(node->right);
+
+      maxDiameter = std::max(maxDiameter, leftHeight + rightHeight);
+
+      return 1 + std::max(leftHeight, rightHeight);
+    };
+
+    dfs(root);
     return maxDiameter;
   }
 };
