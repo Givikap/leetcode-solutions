e3fd6b2c4d0af5734f82d62ce6bf77cd1398252c
Givikap <kaitosekiya@outlook.com>
2026-07-23 19:33:30 -0500

    Add second faster C++ solution for '56. Merge Intervals' problem

--- medium/0056_merge_intervals/solution.cpp
+++ medium/0056_merge_intervals/solution.cpp
@@ -9,14 +9,14 @@ public:
     std::vector<std::vector<int>> mergedIntervals{intervals[0]};
 
     for (size_t i = 1; i < intervals.size(); ++i) {
-      if (mergedIntervals.back()[1] >= intervals[i][0] &&
-          mergedIntervals.back()[1] <= intervals[i][1])
-        mergedIntervals.back()[1] = intervals[i][1];
-      else if (mergedIntervals.back()[0] <= intervals[i][0] &&
-               mergedIntervals.back()[1] >= intervals[i][1])
-        continue;
-      else
+      if (intervals[i][0] <= mergedIntervals.back()[1]) {
+        mergedIntervals.back()[0] =
+            std::min(mergedIntervals.back()[0], intervals[i][0]);
+        mergedIntervals.back()[1] =
+            std::max(mergedIntervals.back()[1], intervals[i][1]);
+      } else {
         mergedIntervals.push_back(intervals[i]);
+      }
     }
 
     return mergedIntervals;
