bd6488d7113624f7fcd636ba7deca0118cbf7a97
Givikap <kaitosekiya@outlook.com>
2026-07-25 14:31:01 -0500

    Add second faster C++ solution for '4. Median of Two Sorted Arrays' problem

--- hard/0004_median_of_two_sorted_arrays/solution.cpp
+++ hard/0004_median_of_two_sorted_arrays/solution.cpp
@@ -4,23 +4,36 @@ class Solution {
 public:
   double findMedianSortedArrays(std::vector<int> &nums1,
                                 std::vector<int> &nums2) {
-    std::vector<int>::iterator it1 = nums1.begin();
-    std::vector<int>::iterator it2 = nums2.begin();
+    if (nums1.size() > nums2.size())
+      swap(nums1, nums2);
 
-    std::vector<int>::iterator prev, curr;
+    const size_t m = nums1.size();
+    const size_t n = nums2.size();
 
-    for (size_t _ = 0; _ < (nums1.size() + nums2.size()) / 2 + 1; ++_) {
-      prev = curr;
+    size_t left = 0;
+    size_t right = m;
 
-      if (it2 == nums2.end() || (it1 != nums1.end() && *it1 < *it2))
-        curr = it1++;
-      else
-        curr = it2++;
+    while (left <= right) {
+      size_t cut1 = (left + right) / 2;
+      size_t cut2 = (m + n) / 2 - cut1;
+
+      int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
+      int r1 = (cut1 == m) ? INT_MAX : nums1[cut1];
+      int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
+      int r2 = (cut2 == n) ? INT_MAX : nums2[cut2];
+
+      if (l1 > r2)
+        right = cut1 - 1;
+      else if (l2 > r1)
+        left = cut1 + 1;
+      else {
+        if ((m + n) % 2 == 0)
+          return (std::max(l1, l2) + std::min(r1, r2)) / 2.0;
+        else
+          return std::min(r1, r2);
+      }
     }
 
-    if ((nums1.size() + nums2.size()) % 2 == 0)
-      return (float)(*prev + *curr) / 2;
-    else
-      return *curr;
+    return 0.0;
   }
 };
