c86715b10082190d7c719f0d751b163739f2aaf9
Givikap <kaitosekiya@outlook.com>
2026-07-29 03:06:04 -0500

    Add third faster C++ solution for '49. Group Anagrams' problem

--- medium/0049_group_anagrams/solution.cpp
+++ medium/0049_group_anagrams/solution.cpp
@@ -1,20 +1,17 @@
-#include <array>
-#include <map>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
 public:
   std::vector<std::vector<std::string>>
   groupAnagrams(std::vector<std::string> &strs) {
-    std::map<std::array<int, 26>, std::vector<std::string>> anagramsMap;
+    std::unordered_map<std::string, std::vector<std::string>> anagramsMap;
 
     for (const std::string &str : strs) {
-      std::array<int, 26> charCounter{};
-      for (const char &ch : str)
-        ++charCounter[ch - 'a'];
-
-      anagramsMap[charCounter].push_back(str);
+      std::string hash(str);
+      std::sort(hash.begin(), hash.end());
+      anagramsMap[hash].push_back(str);
     }
 
     std::vector<std::vector<std::string>> anagramGroups;
