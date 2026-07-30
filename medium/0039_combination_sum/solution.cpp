f81272856b812856c5b2e07261b44dfab6dc7742
Givikap <kaitosekiya@outlook.com>
2026-07-30 01:33:22 -0500

    Add second faster C++ solution for '39. Combination Sum' problem

--- medium/0039_combination_sum/solution.cpp
+++ medium/0039_combination_sum/solution.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <stack>
 #include <tuple>
 #include <vector>
@@ -9,26 +10,31 @@ public:
     const size_t n = candidates.size();
     std::sort(candidates.begin(), candidates.end());
 
-    std::stack<std::tuple<size_t, int, std::vector<int>>> s;
-    s.push({0, 0, {}});
+    std::vector<int> path;
+
+    std::stack<std::tuple<size_t, int, bool>> s;
+    s.push({0, target, false});
 
     std::vector<std::vector<int>> combinations;
 
     while (!s.empty()) {
-      auto [i, sum, combination] = s.top();
-      s.pop();
+      auto &[i, remaining, done] = s.top();
 
-      if (sum == target) {
-        combinations.push_back(combination);
-        continue;
-      }
-
-      for (size_t j = i; j < n; ++j) {
-        if (sum + candidates[j] <= target) {
-          std::vector<int> c(combination);
-          c.push_back(candidates[j]);
-          s.push({j, sum + candidates[j], c});
+      if (!done) {
+        if (remaining == 0) {
+          combinations.push_back(path);
+          s.pop();
+        } else if (i == n || candidates[i] > remaining) {
+          s.pop();
+        } else {
+          path.push_back(candidates[i]);
+          done = true;
+          s.push({i, remaining - candidates[i], 0});
         }
+      } else {
+        path.pop_back();
+        s.pop();
+        s.push({i + 1, remaining, 0});
       }
     }
 
