430183c43fcf5e3650cffc2a47aa977954e90680
Givikap <kaitosekiya@outlook.com>
2026-07-31 15:56:08 -0500

    Add second faster C++ solution for '451. Sort Characters By Frequency' problem

--- medium/0451_sort_characters_by_frequency/solution.cpp
+++ medium/0451_sort_characters_by_frequency/solution.cpp
@@ -1,19 +1,26 @@
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 class Solution {
 public:
   std::string frequencySort(std::string s) {
-    std::unordered_map<char, int> frequencyMap;
+    std::unordered_map<char, size_t> frequencyMap;
     for (const char &ch : s)
       ++frequencyMap[ch];
 
-    std::sort(s.begin(), s.end(), [&](const char &a, const char &b) {
-      if (frequencyMap[a] == frequencyMap[b])
-        return a > b;
+    std::vector<std::pair<char, size_t>> frequencyPairs(frequencyMap.begin(),
+                                                        frequencyMap.end());
+    std::sort(
+        frequencyPairs.begin(), frequencyPairs.end(),
+        [](const std::pair<char, size_t> &a, const std::pair<char, size_t> &b) {
+          return a.second > b.second;
+        });
 
-      return frequencyMap[a] > frequencyMap[b];
-    });
+    s.clear();
+
+    for (const auto &[ch, frequency] : frequencyPairs)
+      s += std::string(frequency, ch);
 
     return s;
   }
