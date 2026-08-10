5adfebd2eca2ed8f7c923449cb66390765275f5c
Givikap <kaitosekiya@outlook.com>
2026-08-10 10:19:44 -0500

    Add second faster Python solution for '3016. Minimum Number of Pushes to Type Word II' problem

--- medium/3016_minimum_number_of_pushes_to_type_word_ii/solution.py
+++ medium/3016_minimum_number_of_pushes_to_type_word_ii/solution.py
@@ -1,14 +1,11 @@
+from collections import Counter
+
+
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        charsCounter = [0] * 26
-        for ch in word:
-            charsCounter[ord(ch) - 97] += 1
-
-        pushesCount = 0
-        keysCount = 0
-
-        for count in sorted(charsCounter, reverse=True):
-            pushesCount += (keysCount // 8 + 1) * count
-            keysCount += 1
-
-        return pushesCount
+        return sum(
+            (i // 8 + 1) * count
+            for i, count in enumerate(
+                sorted(Counter(word).values(), reverse=True)
+            )
+        )
