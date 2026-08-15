072216747d2ab602a2b338ddb3f98730d0b0344d
Givikap <kaitosekiya@outlook.com>
2026-08-15 16:09:20 -0500

    Add first Python solution for '2121. Intervals Between Identical Elements' problem

---
+++ medium/2121_intervals_between_identical_elements/solution.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def getDistances(self, arr: List[int]) -> List[int]:
+        indicesMap = defaultdict(list)
+        for i, num in enumerate(arr):
+            indicesMap[num].append(i)
+
+        numsSums = [0] * len(arr)
+
+        for indices in indicesMap.values():
+            k = len(indices)
+
+            indicesSum = sum(indices)
+            leftSum = 0
+
+            for m, index in enumerate(indices):
+                numsSums[index] = (
+                    indicesSum - 2 * leftSum - index * (k - 2 * m)
+                )
+                leftSum += index
+
+        return numsSums
