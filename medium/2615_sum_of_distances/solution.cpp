c614b1c243b2a4b627b55e0be39311cb3009af40
Givikap <kaitosekiya@outlook.com>
2026-08-15 00:43:46 -0500

    Add second faster C++ solution for '2615. Sum of Distances' problem

--- medium/2615_sum_of_distances/solution.cpp
+++ medium/2615_sum_of_distances/solution.cpp
@@ -15,23 +15,17 @@ public:
     std::vector<long long> numsSums(n, 0);
 
     for (auto &[_, indices] : indicesMap) {
-      std::sort(indices.begin(), indices.end());
+      sort(indices.begin(), indices.end());
 
       size_t k = indices.size();
 
       std::vector<long long> prefixSums(k + 1, 0);
-      for (size_t m = 1; m <= k; ++m)
+      for (size_t m = 1; m < k + 1; ++m)
         prefixSums[m] = prefixSums[m - 1] + indices[m - 1];
 
-      for (size_t m{}; m < k; ++m) {
-        long long index = indices[m];
-
-        long long leftSum = index * static_cast<long long>(m) - prefixSums[m];
-        long long rightSum = (prefixSums[k] - prefixSums[m] - index) -
-                             index * static_cast<long long>(k - 1 - m);
-
-        numsSums[index] = leftSum + rightSum;
-      }
+      for (size_t m{}; m < k; ++m)
+        numsSums[indices[m]] = prefixSums[k] - 2 * prefixSums[m] -
+                               indices[m] * static_cast<long long>(k - 2 * m);
     }
 
     return numsSums;
