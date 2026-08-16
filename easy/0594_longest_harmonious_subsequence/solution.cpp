6c234cd337c5ff5f5529bb094950534cf9a7b0f2
Givikap <kaitosekiya@outlook.com>
2026-08-16 00:01:58 -0500

    Add second faster C++ solution for '594. Longest Harmonious Subsequence' problem

--- easy/0594_longest_harmonious_subsequence/solution.cpp
+++ easy/0594_longest_harmonious_subsequence/solution.cpp
@@ -1,18 +1,18 @@
-#include <map>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
 public:
   int findLHS(std::vector<int> &nums) {
-    std::map<int, int> numsCounter;
+    std::unordered_map<int, int> numsCounter;
     for (const int &num : nums)
       ++numsCounter[num];
 
     int maxLen = 0;
 
-    for (auto it = numsCounter.begin(); it != numsCounter.end(); ++it) {
-      if (next(it) != numsCounter.end() && next(it)->first - it->first == 1)
-        maxLen = std::max(maxLen, it->second + next(it)->second);
+    for (const auto &[num, count] : numsCounter) {
+      if (numsCounter.find(num + 1) != numsCounter.end())
+        maxLen = std::max(maxLen, numsCounter[num] + numsCounter[num + 1]);
     }
 
     return maxLen;
