a8fba6b59fab4b0b1769fe59208986ca1de153f4
Givikap <kaitosekiya@outlook.com>
2026-08-19 18:32:40 -0500

    Add second faster C++ solution for '494. Target Sum' problem

--- medium/0494_target_sum/solution.cpp
+++ medium/0494_target_sum/solution.cpp
@@ -1,3 +1,4 @@
+#include <numeric>
 #include <unordered_map>
 #include <vector>
 
@@ -5,19 +6,22 @@ class Solution {
 public:
   int findTargetSumWays(std::vector<int> &nums, int target) {
     const size_t n = nums.size();
+    const size_t offset =
+        static_cast<size_t>(std::accumulate(nums.begin(), nums.end(), 0));
 
-    std::unordered_map<size_t, std::unordered_map<int, int>> memo;
+    std::vector<std::vector<int>> memo(n + 1,
+                                       std::vector<int>(2 * offset + 1, -1));
 
     auto solve = [&](this auto &&self, int curr, size_t i) -> int {
       if (i == n)
         return curr == target ? 1 : 0;
 
-      auto it = memo[i].find(curr);
-      if (it != memo[i].end())
-        return it->second;
+      size_t j = static_cast<size_t>(curr) + offset;
+      if (memo[i][j] != -1)
+        return memo[i][j];
 
-      memo[i][curr] = self(curr + nums[i], i + 1) + self(curr - nums[i], i + 1);
-      return memo[i][curr];
+      memo[i][j] = self(curr + nums[i], i + 1) + self(curr - nums[i], i + 1);
+      return memo[i][j];
     };
 
     return solve(0, 0);
