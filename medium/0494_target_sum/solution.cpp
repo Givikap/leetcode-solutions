ef8a7c36e96d5528da32e8c4dbecaca6bd0c9382
Givikap <kaitosekiya@outlook.com>
2026-08-20 18:14:26 -0500

    Add third C++ solution for '494. Target Sum' problem

--- medium/0494_target_sum/solution.cpp
+++ medium/0494_target_sum/solution.cpp
@@ -1,4 +1,5 @@
 #include <numeric>
+#include <stack>
 #include <unordered_map>
 #include <vector>
 
@@ -7,23 +8,34 @@ public:
   int findTargetSumWays(std::vector<int> &nums, int target) {
     const size_t n = nums.size();
     const size_t offset =
-        static_cast<size_t>(std::accumulate(nums.begin(), nums.end(), 0));
+        static_cast<size_t>(accumulate(nums.begin(), nums.end(), 0));
 
     std::vector<std::vector<int>> memo(n + 1,
                                        std::vector<int>(2 * offset + 1, -1));
 
-    auto solve = [&](this auto &&self, int curr, size_t i) -> int {
-      if (i == n)
-        return curr == target ? 1 : 0;
+    std::stack<std::pair<size_t, int>> st;
+    st.push({0, 0});
 
-      size_t j = static_cast<size_t>(curr) + offset;
-      if (memo[i][j] != -1)
-        return memo[i][j];
+    while (!st.empty()) {
+      auto [i, curr] = st.top();
 
-      memo[i][j] = self(curr + nums[i], i + 1) + self(curr - nums[i], i + 1);
-      return memo[i][j];
-    };
+      if (i == n) {
+        memo[i][curr + offset] = curr == target ? 1 : 0;
+        st.pop();
+      } else {
+        if (memo[i + 1][curr - nums[i] + offset] == -1)
+          st.push({i + 1, curr - nums[i]});
+        if (memo[i + 1][curr + nums[i] + offset] == -1)
+          st.push({i + 1, curr + nums[i]});
 
-    return solve(0, 0);
+        if (st.top().first == i) {
+          memo[i][curr + offset] = memo[i + 1][curr + nums[i] + offset] +
+                                   memo[i + 1][curr - nums[i] + offset];
+          st.pop();
+        }
+      }
+    }
+
+    return memo[0][offset];
   }
 };
