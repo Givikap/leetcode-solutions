89b2cf776c77cf923c75ab3ee1ebb89a02c73b5f
Givikap <kaitosekiya@outlook.com>
2026-08-22 01:36:30 -0500

    Add third faster C++ solution for '221. Maximal Square' problem

--- medium/0221_maximal_square/solution.cpp
+++ medium/0221_maximal_square/solution.cpp
@@ -6,20 +6,22 @@ public:
     const size_t rows = matrix.size();
     const size_t cols = matrix[0].size();
 
-    std::vector<std::vector<int>> dp(rows, std::vector<int>(cols, 0));
+    std::vector<int> dp(cols, 0);
 
+    int prev = 0;
     int maxSize = 0;
 
     for (size_t row{}; row < rows; ++row) {
       for (size_t col{}; col < cols; ++col) {
-        if (row == 0 || col == 0 || matrix[row][col] == '0')
-          dp[row][col] = matrix[row][col] - '0';
-        else
-          dp[row][col] = std::min({dp[row - 1][col - 1], dp[row - 1][col],
-                                   dp[row][col - 1]}) +
-                         1;
+        int temp = dp[col];
 
-        maxSize = std::max(maxSize, dp[row][col]);
+        if (row == 0 || col == 0 || matrix[row][col] == '0')
+          dp[col] = matrix[row][col] - '0';
+        else
+          dp[col] = std::min({prev, dp[col], dp[col - 1]}) + 1;
+
+        prev = temp;
+        maxSize = std::max(maxSize, dp[col]);
       }
     }
 
