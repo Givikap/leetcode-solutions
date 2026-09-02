6d69e585602375617c0bf66563377c907e617ddd
Givikap <kaitosekiya@outlook.com>
2026-09-02 00:05:58 -0500

    Add first Python solution for '1232. Check If It Is a Straight Line' problem

---
+++ easy/1232_check_if_it_is_a_straight_line/solution.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        for i in range(2, len(coordinates)):
+            if (coordinates[i - 1][1] - coordinates[i - 2][1]) * (
+                coordinates[i][0] - coordinates[i - 2][0]
+            ) != (coordinates[i][1] - coordinates[i - 2][1]) * (
+                coordinates[i - 1][0] - coordinates[i - 2][0]
+            ):
+                return False
+
+        return True
