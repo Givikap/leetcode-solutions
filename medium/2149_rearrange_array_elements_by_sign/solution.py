8d60cb18c76a825ed528dc9069aba8d7c720b7f7
Givikap <kaitosekiya@outlook.com>
2026-09-04 00:28:08 -0500

    Add first Python solution for '2149. Rearrange Array Elements by Sign' problem

---
+++ medium/2149_rearrange_array_elements_by_sign/solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        rearrangedNums = [0] * len(nums)
+
+        positive = 0
+        negative = 1
+
+        for num in nums:
+            if num > 0:
+                rearrangedNums[positive] = num
+                positive += 2
+            else:
+                rearrangedNums[negative] = num
+                negative += 2
+
+        return rearrangedNums
