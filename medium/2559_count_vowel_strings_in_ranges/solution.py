3fd3916946ce2add839e924fac7b5c6d9937ecef
Givikap <kaitosekiya@outlook.com>
2026-09-05 12:56:33 -0500

    Add first Python solution for '2559. Count Vowel Strings in Ranges' problem

---
+++ medium/2559_count_vowel_strings_in_ranges/solution.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def vowelStrings(
+        self, words: List[str], queries: List[List[int]]
+    ) -> List[int]:
+        vowels = "aeiou"
+
+        prefixSums = [0] * (len(words) + 1)
+
+        for i in range(1, len(words) + 1):
+            prefixSums[i] = prefixSums[i - 1] + (
+                words[i - 1][0] in vowels and words[i - 1][-1] in vowels
+            )
+
+        results = []
+
+        for li, ri in queries:
+            results.append(prefixSums[ri + 1] - prefixSums[li])
+
+        return results
