f6051465214e49f20f9ef5490b520585292d0524
Givikap <kaitosekiya@outlook.com>
2026-09-07 21:40:58 -0500

    Add first Python solution for '36. Valid Sudoku' problem

---
+++ medium/0036_valid_sudoku/solution.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rowsMap = [[False] * 9 for _ in range(9)]
+        colsMap = [[False] * 9 for _ in range(9)]
+        boxesMap = [[False] * 9 for _ in range(9)]
+
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == ".":
+                    continue
+
+                di = ord(board[row][col]) - 49
+                if rowsMap[row][di] or colsMap[col][di]:
+                    return False
+
+                bi = 3 * (row // 3) + col // 3
+                if boxesMap[bi][di]:
+                    return False
+
+                rowsMap[row][di] = True
+                colsMap[col][di] = True
+                boxesMap[bi][di] = True
+
+        return True
