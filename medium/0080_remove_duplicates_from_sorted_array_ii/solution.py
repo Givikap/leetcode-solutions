014002fa4a90d2c7a4af017759ce2b3fc23dc3c5
Givikap <kaitosekiya@outlook.com>
2026-09-08 17:00:04 -0500

    Add second faster Python solution for '80. Remove Duplicates from Sorted Array II' problem

--- medium/0080_remove_duplicates_from_sorted_array_ii/solution.py
+++ medium/0080_remove_duplicates_from_sorted_array_ii/solution.py
@@ -3,18 +3,17 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        n = len(nums)
+        if len(nums) <= 2:
+            return len(nums)
 
-        insert = 0
-        read = 0
+        insert = 2
 
-        while read < n:
-            nums[insert] = nums[read]
-            read += 1
-
-            while read + 1 < n and nums[read + 1] == nums[insert]:
-                read += 1
-
-            insert += 1
+        for read in range(2, len(nums)):
+            if (
+                nums[read] != nums[insert - 1]
+                or nums[read] != nums[insert - 2]
+            ):
+                nums[insert] = nums[read]
+                insert += 1
 
         return insert
